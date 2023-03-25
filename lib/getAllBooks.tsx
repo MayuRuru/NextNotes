b6503c930296c50e6af2805a2741b57e183ba555
MayuRuru <aruizrull@protonmail.com>
2023-03-25 17:25:45 +0100

    Add jsonplacholder for fethAll

--- lib/getAllBooks.tsx
+++ lib/getAllBooks.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default async function getAllBooks() {
-  const res = await fetch("");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
   if (!res.ok) throw new Error("failed to fecth data");
 
