2dbacee5352a088f5d29a96eaf445ab8ff1e90f9
MayuRuru <aruizrull@protonmail.com>
2023-03-26 20:24:32 +0200

    Change error to undefined

--- lib/getBookInsights.tsx
+++ lib/getBookInsights.tsx
@@ -2,7 +2,8 @@ export default async function getBookInsights(bookId: string) {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?bookId=/${bookId}`
   );
-  if (!res.ok) throw new Error("faled to fecth book");
+  //if (!res.ok) throw new Error("faled to fecth book");
+  if (!res.ok) return undefined;
 
   return res.json();
 }
