ac5ab5f25dd826c6ee7cea579e1a13871c87d9df
MayuRuru <aruizrull@protonmail.com>
2023-03-27 12:36:42 +0200

    Create Navbar component and display in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,9 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="dark:bg-slate-900">
         <nav>
-          <h1>Navbar</h1>
+          <Navbar />
         </nav>
         {children}
       </body>
