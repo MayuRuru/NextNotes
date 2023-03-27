cf2cd93ff93db3a2b743830586c4d5ad95c21f3c
MayuRuru <aruizrull@protonmail.com>
2023-03-27 12:36:24 +0200

    Apply styles to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import Image from "next/image";
+//import Image from "next/image";
 import Link from "next/link";
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello there</h1>
-      <p>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello there! &nbsp;
+        <span className="whitespace-nowrap">
+          This is <span className="font-bold">Mayu</span>.
+        </span>
+        <br />
         <Link href="/about">Go to About</Link>
         <br />
         <Link href="/books">Link to Books</Link>
