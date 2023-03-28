f0dade5a2b2ac781e718d8e372cec287ae685f89
MayuRuru <aruizrull@protonmail.com>
2023-03-28 10:34:46 +0200

    Reorder

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        Hello there! &nbsp;
+        Hello there!
         <span className="whitespace-nowrap">
           This is <span className="font-bold">Mayu</span>.
         </span>
