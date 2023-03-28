148e3ebc17ec70df2cf863a301b8cfa301284de1
MayuRuru <aruizrull@protonmail.com>
2023-03-28 15:36:50 +0200

    StaticParas generator to make Post page SSG

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -3,6 +3,14 @@ import { getPostData, getSortedPostsData } from "@/lib/posts";
 import { notFound } from "next/navigation";
 import getFormattedDate from "@/lib/getFormattedDate";
 
+export function generateStaticParams() {
+  const posts = getSortedPostsData();
+
+  return posts.map((post) => ({
+    postId: post.id,
+  }));
+}
+
 export function generateMetadata({ params }: { params: { postId: string } }) {
   const posts = getSortedPostsData();
   const { postId } = params;
