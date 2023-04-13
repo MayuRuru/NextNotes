42dce69dc5f4766cbd04bb11a7f93c3f8ddda693
MayuRuru <aruizrull@protonmail.com>
2023-04-13 10:10:03 +0200

    Refactor Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,52 +1,49 @@
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
-//fixed w-full h-20 z-50 (prev navbar)
+const links = [
+  { route: "/", label: "Home" },
+  { route: "/about", label: "About" },
+  { route: "/books", label: "Books" },
+  { route: "/creators", label: "Creators" },
+];
+
+const icons = [
+  {
+    href: "https://www.linkedin.com/in/ruiz-rull/",
+    icon: <FaLinkedin />,
+  },
+  {
+    href: "https://github.com/MayuRuru/",
+    icon: <FaGithub />,
+  },
+];
 
 export default function Navbar() {
   return (
     <nav className="bg-navbar p-1 flex flex-col md:flex-row justify-between items-center top-0 drop-shadow-x1 align-middle rounded border-slate-100 mx-auto">
-      {/*LINKS:*/}
       <h3 className="font-bold text-white place-content-center mb-2 md:mb-0 flex flex-row gap-8 m-4 text-2xl">
-        <Link
-          href="/"
-          className="text-white/90 hover:opacity-80 no-underline hover:text-white"
-        >
-          Home
-        </Link>
-        <Link
-          href="/about"
-          className="text-white/90 hover:opacity-80 no-underline hover:text-white"
-        >
-          About
-        </Link>
-        <Link
-          href="/books"
-          className="text-white/90 hover:opacity-80 no-underline hover:text-white"
-        >
-          Books
-        </Link>
-        <Link
-          href="/creators"
-          className="text-white/90 hover:opacity-80 no-underline hover:text-white"
-        >
-          Creators
-        </Link>
+        {links.map(({ route, label }) => (
+          <Link
+            key={label}
+            href={route}
+            className="text-white/90 hover:opacity-80 no-underline hover:text-white"
+          >
+            {label}
+          </Link>
+        ))}
       </h3>
-      {/*ICONS:*/}
+
       <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-3xl m-4">
-        <Link
-          className="text-white/90 hover:text-white"
-          href="https://www.linkedin.com/in/ruiz-rull/"
-        >
-          <FaLinkedin />
-        </Link>
-        <Link
-          className="text-white/90 hover:text-white"
-          href="https://github.com/MayuRuru/"
-        >
-          <FaGithub />
-        </Link>
+        {icons.map(({ href, icon }) => (
+          <Link
+            key={href}
+            href={href}
+            className="text-white/90 hover:text-white"
+          >
+            {icon}
+          </Link>
+        ))}
       </div>
     </nav>
   );
