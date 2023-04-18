fe6fa88472b27aab0be814086c141f90cec6b12b
MayuRuru <aruizrull@protonmail.com>
2023-04-18 18:33:23 +0200

    Delete unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Avatar from "./components/Avatar";
-import Posts from "./creators/page";
-import { Inter } from "next/font/google";
 import CardSlider from "./carousel/CardSlider";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={inter.className}>
+    <main>
       <section className="mb-12 flex flex-col items-center">
         <h2 className="text-blue text-4xl font-bold pb-6">
           My highlighted references:
