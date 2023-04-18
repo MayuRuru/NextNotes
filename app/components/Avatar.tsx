74be63abc3641d6ed0850c188f5181908d55402f
MayuRuru <aruizrull@protonmail.com>
2023-04-18 20:01:23 +0200

    Change size of avatar and global font

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
+// TODO: I dont think this should be a separated component
 export default function Avatar() {
   return (
     <Image
-      //className="rounded-full object-cover object-center"
       src="/avatar/bgs.jpg"
       width={200}
       height={200}
       alt="avatar"
-      priority={true}
+      priority
       style={{ aspectRatio: "1 / 1", width: "100%", height: "auto" }}
     />
   );
